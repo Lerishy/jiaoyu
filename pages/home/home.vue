e15e7cac12be8121a4693c4d251dde5088119ed0
Lerishy <2230008711@qq.com>
2022-12-01 22:37:18 +0800

    登录功能

---
+++ pages/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		 onLoad() {
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.aa{
+	width: 300rpx;
+	height: 300rpx;
+	background-color: aqua;
+}
+</style>
